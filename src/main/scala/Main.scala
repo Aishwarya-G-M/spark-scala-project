4c308068f9d4bffd1bec37ae023fbc5f6d364cd2
AishwaryaGM <aishwaryagm@AishwaryaGMs-MacBook-Pro.local>
2026-05-05 18:26:37 +0530

    ways to reference columns

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 package de.agm.sparkscalaproject
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -10,12 +11,27 @@ object Main {
       .config("spark.driver.bindAddress","127.0.0.1")
       .getOrCreate()
 
-    val df = spark.read
+    val df: DataFrame= spark.read
       .option("header",value = true)
       .option("inferSchema",value = true)
       .csv("data/AAPL.csv")
 
     df.show()
     df.printSchema()
+
+    // ways to select columns from our dataframes
+    df.select("Date","Open","Close").show()
+    // using object column
+    val column = df("Date")
+    // using a function
+    col("Date")
+    // import spark implicits
+    import spark.implicits._
+    $"Date"
+
+    //df("Close") -> uses the apply
+    //df.select(col("Date"), $"Open", df("Close")).show()
+    // or
+    df.select(column, $"Open", df("Close")).show()
   }
 }
