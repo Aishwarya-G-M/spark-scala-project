f26af45f98dbe867cd3b37e2e841881822407444
AishwaryaGM <aishwaryagm@AishwaryaGMs-MacBook-Pro.local>
2026-05-05 16:07:01 +0530

    Initial running spark app

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,19 @@
+package de.agm.sparkscalaproject
+
+import org.apache.spark.sql.SparkSession
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("spark-scala-project")
+      .master("local[*]")
+      .config("spark.driver.bindAddress","127.0.0.1")
+      .getOrCreate()
+
+    val df = spark.read
+      .option("header",value = true)
+      .csv("data/AAPL.csv")
+
+    df.show()
+  }
+}
