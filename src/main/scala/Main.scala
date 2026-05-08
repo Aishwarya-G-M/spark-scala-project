c2c3feb6c9a549de6e32c0289e38235e125c6298
AishwaryaGM <aishwaryagm@AishwaryaGMs-MacBook-Pro.local>
2026-05-08 09:07:10 +0530

    col,lit,concat functions

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,8 @@
 package de.agm.sparkscalaproject
 
-import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.functions.{col, lit}
 import org.apache.spark.sql.types.StringType
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame, SparkSession, functions}
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -40,10 +40,14 @@ object Main {
     // column2 + (2.0)
     val columnString = column2.cast(StringType)
 
-    df.select(column2,newColumn,columnString)
-      .filter(newColumn > 2.0)
-      .filter(newColumn > column2)
-      .filter(newColumn === column2)
-      .show()
+    val newColumnString = functions.concat(columnString, lit("Hello world"))
+
+//    df.select(column2,newColumn,columnString)
+//      .filter(newColumn > 2.0)
+//      .filter(newColumn > column2)
+//      .filter(newColumn === column2)
+//      .show()
+
+    df.select(column2, newColumn, columnString,newColumnString).show(truncate = false)
   }
 }
